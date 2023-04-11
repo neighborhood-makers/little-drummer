748edbc0ca1d66169db83e3e14db2d6256cc5645
Neighborhood Makers <53877723+neighborhood-makers@users.noreply.github.com>
2023-04-11 13:57:02 -0400

    make it quicker

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+hummingbird.startHummingbird()
 basic.forever(function () {
-	
+    hummingbird.setPositionServo(FourPort.One, 140)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    hummingbird.setPositionServo(FourPort.One, 180)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(200)
 })
